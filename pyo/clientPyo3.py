d52458e6a3c7dc3a0059c8a33680a30b4c93436e
h2s-speleo <h2s.speleo@gmail.com>
2020-04-17 15:42:25 +0200

    3° version du serveur pyo

---
+++ pyo/clientPyo3.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 19:04:55 2020
+
+@author: j
+"""
+
+import time
+from pickle import load
+from io import BytesIO
+import pika
+
+class PyoClient():
+    def __init__(self):
+        self.etatConn = 0
+        self.crash = 0
+        self.initMess = list()
+        self.millisEnv = int(round(time.time() * 1000))
+        self.enAttente = 0
+        self.run()
+    
+    def run(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+
+        self.channel.queue_purge(queue = 'rpcPyo')
+        result = self.channel.queue_declare(queue='', exclusive=True)
+
+        self.callback_queue = result.method.queue
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+        print('server run')
+
+    def on_response(self, ch, method, props, body):
+        self.response = body
+
+    def initPypObject(self):
+        self.etatConn = 1
+        for i in range(len(self.initMess)):
+            pyo_rpc.send(self.initMess[i])
+            
+    def testConnex(self):
+        compt = 0
+        while pyo_rpc.etatConn == 0:
+            print ('TESTCONNEX', end = ' -> ')
+            self.millisEnv = int(round(time.time() * 1000))
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='rpcPyo',
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue),
+                    body=str('TESTCONNEX'))
+            self.response = 'en attente'
+            self.enAttente = 1
+            self.millisEnv = time.time() * 1000
+            x = self.reception('TESTCONNEX')
+            print(" -> %r" % x)
+            if x[1] == 1:
+                self.initPypObject()
+                return x
+                break
+            compt = compt + 1
+            if compt > 5 :
+                print('impossible de se connecter')
+                break
+            time.sleep(0.2)
+    
+    def send(self, comm):
+        if self.etatConn == 0:
+            print('connexion morte, test de la connection')
+            x =self.testConnex()
+        else :
+            print (comm, end = ' -> ')
+            self.millisEnv = time.time() * 1000
+            self.channel.basic_publish(
+                exchange='',
+                routing_key='rpcPyo',
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue),
+                    body=str(comm))
+            self.response = 'en attente'
+            self.enAttente = 1
+            x = self.reception(comm)       
+            print(" -> %r" % x)
+            print("TU ME FAIS CHIER AVEC TON PUTAIN DE SERVEUR")
+            if x[1] == 'CRASHSERVER':
+                self.etatConn = 0
+        return x
+        
+    def checkReponse(self):
+        if self.enAttente == 1 :
+            attente = (time.time() * 1000) - self.millisEnv
+            if attente > 600 :
+                print(str(attente)[:4], end = ' ')
+                self.enAttente = 0 
+                print('CRASHSERVER')
+                return 'CRASHSERVER'
+            elif self.response == 'en attente' :
+                self.connection.process_data_events()
+                return 'en attente'
+            else :
+                self.enAttente = 0
+                print(str(attente)[:4], end = ' ')
+                return self.response
+        else :
+            return 'rien a faire'
+        
+    def reception(self, comm):
+        while True :
+            response = self.checkReponse()
+            if response != 'rien a faire' and response != 'en attente':
+                if type(response) == bytes:
+                    listeRep = self.deSerialiser(response)
+                else:
+                    listeRep = [comm, 'CRASHSERVER']
+                return listeRep
+
+    def deSerialiser(self, seq):
+        buffer = BytesIO(seq)
+        read = load(buffer)
+        return read
+    
+    def kill(self):
+        self.send('break')
+        try :
+            self.channel.queue_delete(queue='rpcPyo')
+        except:
+            pass
+        try :
+            self.channel.close()
+        except :
+            pass
+        try :
+            self.connection.close()
+        except :
+            pass
+        try :
+            self.connection.close()
+        except:
+            pass
+
+if __name__ == '__main__':
+    pyo_rpc = PyoClient()
+    pyo_rpc.initMess.append('a = Sine()')
+    pyo_rpc.testConnex()
+    
+#    listeCommande = ['a.out()','RETVALUE= a._freq', 'a.stop()']
+#    for j in range(3):
+#        for i in range(len(listeCommande)):
+#            x = pyo_rpc.send(listeCommande[i])
+#            time.sleep(1)
+#    
+#    listeCommande = ['REBOOT', 'iusdfiougqsdfoiug']
+#    for j in range(2):
+#        for i in range(len(listeCommande)):
+#            x = pyo_rpc.send(listeCommande[i])
+#            time.sleep(1)
+#    
+#    listeCommande = ['a.out()','RETVALUE= a._freq', 'a.stop()']
+#    for j in range(3):
+#        for i in range(len(listeCommande)):
+#            x = pyo_rpc.send(listeCommande[i])
+#            time.sleep(1)
+
+    pyo_rpc.kill()
---
+++ pyo/serverPyo3.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 22:16:00 2020
+
+@author: j
+"""
+import os
+import time
+from signal import SIGTERM, SIGKILL
+from multiprocessing import Process, freeze_support, Queue
+from pickle import dump
+from io import BytesIO
+import pika
+from pyo import *
+
+
+def ServPyo(self):
+    s = Server() #instanciation du serveur
+    x = None
+    print(self.audioDevice)
+    inputs, outputs = pa_get_devices_infos() #récupération de la liste des périphériques
+    for index in sorted(outputs.keys()):
+        if outputs[index]['name'] == self.audioDevice : # nom de ton périférique son
+            x = index
+    if x == None : #en cas d'échec on se choisi au périphérique 0
+        print('default device')
+        x = 1
+    s.setOutputDevice(x) #définition de la sortie audio du serveur
+    while True :
+        if not self.qSonIN2.empty(): #si on a quelquechose dans la queue
+            RETVALUE = 'test'
+            mes2 = self.qSonIN2.get()
+            if 'RETVALUE' in mes2 :
+                loc = {}
+                exec(mes2, locals(), loc)
+                RETVALUE = loc.get('RETVALUE')
+                self.qSonOUT.put(RETVALUE)
+            else :
+                if mes2 == 'break':
+                    break
+                else:
+                    exec(mes2)
+                    self.qSonOUT.put('NULL')
+        time.sleep(0.001)#ralenti la boucle principale
+    s.stop() #on coupe le serveur
+    print('coupure Serveur Pyo')
+
+class rpcServeurPyo():
+    def __init__(self):
+        self.etatCon = 0
+        self.audioD()
+        freeze_support()
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='rpcPyo')
+        self.channel.queue_purge(queue = 'rpcPyo')
+        self.channel.basic_qos(prefetch_count=10)
+        self.channel.basic_consume(queue='rpcPyo', on_message_callback=self.on_request)
+        self.run()
+        print('____________________________________________')
+        self.channel.start_consuming()
+
+    def run(self):
+        self.qSonIN2 = Queue() #création de la queue du serveur pyo
+        self.qSonOUT = Queue() #création de la queue du serveur pyo
+        while not self.qSonIN2.empty():
+            self.qSonIN2.get()
+        while not self.qSonOUT.empty():
+            self.qSonOUT.get()
+        self.channel.queue_purge(queue = 'rpcPyo')
+        self.SP = Process(target=ServPyo, daemon=True, args=(self,)) #création du processus du serveur pyo
+        self.SP.start()
+        self.pidServPyo = self.SP.pid
+        print('#############################')
+        print('PID serveur Pyo : ', end = '')
+        print(self.pidServPyo)
+        print('#############################')
+        self.initmessage()
+        self.etatCon = 1
+        print('serveur initialisé')
+
+    def kill(self):
+        self.qSonIN2.put('break')
+        self.etatCon = 0
+        try :
+            self.qSonIN2.close()
+        except:
+            pass
+        try:
+            self.qSonOUT.close()
+        except:
+            pass
+        try:
+            self.SP.terminate()
+            self.SP.join()
+            self.SP.close()
+        except : 
+            try:
+                os.kill(self.pidServPyo, SIGTERM)
+                print('TERM signal')
+                os.kill(self.pidServPyo, SIGKILL)
+                print('KILL signal')
+            except:
+                pass
+        
+        print('kill')
+  
+    def reboot(self):
+        print('reboot serveur pyo')
+        self.kill()
+        self.run()
+     
+    def initmessage(self):
+        listeInit = ['s.boot()','s.start()']
+        for i in range(len(listeInit)):
+            print(listeInit[i], end = '  ')
+            self.qSonIN2.put(listeInit[i])
+            self.check_message()
+            rep = self.qSonOUT.get()
+            print('| rep', end = ' ')
+            print(rep)
+
+    def on_request(self, ch, method, props, body):
+        mess = body.decode('ascii')
+        print(mess, end = '  ')
+        if mess == 'TESTCONNEX' :
+            if self.etatCon == 1:
+                self.qSonIN2.put('RETVALUE = s.getIsStarted()')
+                self.check_message()
+                rep = self.qSonOUT.get()
+                print('| rep', end = ' ')
+                print(rep)
+                if rep == 0:
+                    self.initmessage()
+            else:
+                self.reboot()
+            time.sleep(0.1)
+            DATA = self.serialiser([str(mess), self.etatCon])
+            self.channel.basic_publish(exchange='',
+                             routing_key=props.reply_to,
+                             properties=pika.BasicProperties(),
+                             body=DATA)
+            self.channel.basic_ack(delivery_tag=method.delivery_tag)
+        else :
+            self.qSonIN2.put(mess)
+            if mess == 'break' :
+                self.kill()
+                DATA = self.serialiser([str(mess), 'SERVEUR_MORT'])
+                self.channel.basic_publish(exchange='',
+                                 routing_key=props.reply_to,
+                                 properties=pika.BasicProperties(),
+                                 body=DATA)
+                self.channel.basic_ack(delivery_tag=method.delivery_tag)
+                self.channel.stop_consuming()
+                
+            elif mess == 'REBOOT' :
+                self.kill()
+                DATA = self.serialiser([str(mess), 'REBOOTING'])
+                self.channel.basic_publish(exchange='',
+                                 routing_key=props.reply_to,
+                                 properties=pika.BasicProperties(),
+                                 body=DATA)
+                self.channel.basic_ack(delivery_tag=method.delivery_tag)
+                self.reboot()
+                
+            else :
+                self.check_message()
+                rep = self.qSonOUT.get()           
+                DATA = self.serialiser([str(mess), rep])
+                self.channel.basic_publish(exchange='',
+                                 routing_key=props.reply_to,
+                                 properties=pika.BasicProperties(),
+                                 body=DATA)
+                self.channel.basic_ack(delivery_tag=method.delivery_tag)
+                print('| rep', end = ' ')
+                print(rep)
+                if rep == 'CRASHSERVER':
+                    self.reboot() 
+
+    def check_message(self):
+        temps = time.time() * 1000
+        while self.qSonOUT.empty():
+            time.sleep(0.001)  
+            if (time.time() * 1000) - temps  > 500 :
+                self.etatCon = 0
+                self.qSonOUT.put('CRASHSERVER')
+                break
+        print('| delay', end = ' : ')
+        print( str((time.time() * 1000) - temps  )[:4], end = ' ')
+
+    def serialiser(self, data):
+        buffer = BytesIO()
+        res = dump(data, buffer)
+        seq = buffer.getvalue()
+        return seq
+    
+    def audioD(self):
+        with open("./conf", "r") as f :
+            fichier_entier = f.read()
+            files = fichier_entier.split("\n")
+        for line in files :
+            if line.startswith('audio device ='):
+                var = line.replace('audio device =', '')
+                break
+        while var.startswith(' '):
+            var = var[1:]
+        while var.endswith(' '):
+            var = var[:-1]
+        self.audioDevice =  var
+    
+
+     
+if __name__ == '__main__':
+    RPC = rpcServeurPyo()
+    RPC.channel.queue_delete(queue='rpcPyo')
+    RPC.channel.close()
+    RPC.connection.close()
